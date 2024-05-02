60738bea055d20a9f80bab847cc49fb874a3bb73
Reuben <treuben90@yahoo.com>
2024-05-02 06:01:17 +0300

    created the user contacrt form and stytled it

--- src/Components/Testimonials&Contacts/Contact/Contact.jsx
+++ src/Components/Testimonials&Contacts/Contact/Contact.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import "./Contact.css";
 
+const contactDetails = [
+  "Personal Information",
+  "Service requested",
+  "Project details",
+];
 function Contact() {
-  return <div className="contact_container"></div>;
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <div className="contact_container">
+      <div className="indicator_container">
+        {contactDetails.map((item, i) => {
+          return (
+            <div
+              className={activeTab === i ? "detailsNameAndNumber" : "hideTab"}
+              key={i}
+            >
+              <p className="count">
+                {i + 1}/ {contactDetails.length}
+              </p>
+              <p className="item">{item}</p>
+            </div>
+          );
+        })}
+        <div className="indicator">
+          {contactDetails.map((_, index) => {
+            return (
+              <>
+                <button
+                  className={activeTab === index ? "circle" : "noCircle"}
+                  key={index}
+                ></button>
+                <button className="straight_line"></button>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
