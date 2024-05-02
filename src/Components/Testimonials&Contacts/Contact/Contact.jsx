53ca7e033d1063921183f2da107020b08a77c34b
Reuben <treuben90@yahoo.com>
2024-05-03 00:54:12 +0300

    continued styling the contact form

--- src/Components/Testimonials&Contacts/Contact/Contact.jsx
+++ src/Components/Testimonials&Contacts/Contact/Contact.jsx
@@ -10,6 +10,7 @@ function Contact() {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <div className="contact_container">
+      {/* Carousel space */}
       <div className="indicator_container">
         {contactDetails.map((item, i) => {
           return (
@@ -32,11 +33,32 @@ function Contact() {
                   className={activeTab === index ? "circle" : "noCircle"}
                   key={index}
                 ></button>
-                <button className="straight_line"></button>
+                <button
+                  className={
+                    contactDetails.length - 1 === index
+                      ? "hideTab"
+                      : "straight_line"
+                  }
+                ></button>
               </>
             );
           })}
         </div>
+      </div>
+      {/* user form space */}
+      <div className="formSpace">
+        <form action="" className="form">
+          <input type="text" className="input" placeholder="FISRT NAME" />
+          <input type="text" className="input" placeholder="LAST NAME" />
+          <input type="text" className="input" placeholder="COMPANY NAME" />
+          <input type="text" className="input" placeholder="PHONE NUMBER" />
+          <input type="email" className="input" placeholder="EMAIL" />
+          <input
+            type="text"
+            className="input"
+            placeholder="YOUR BUDGET RANGE"
+          />
+        </form>
       </div>
     </div>
   );
