8f3cf9105f22620388b40dcd7a4741316f6bbd52
Reuben <treuben90@yahoo.com>
2024-05-06 03:39:16 +0300

    created the final styling for the contact form

--- src/Components/Testimonials&Contacts/Contact/Contact.jsx
+++ src/Components/Testimonials&Contacts/Contact/Contact.jsx
@@ -11,8 +11,19 @@ const contactDetails = [
 ];
 
 function Contact() {
-  console.log(services);
   const [activeTab, setActiveTab] = useState(0);
+
+  const handleNextCarousel = () => {
+    setActiveTab((prev) =>
+      prev < contactDetails.length - 1 ? prev + 1 : contactDetails.length - 1
+    );
+  };
+
+  const handlePreviousCarousel = () => {
+    setActiveTab((prev) => (prev > 0 ? prev - 1 : 0));
+  };
+
+  console.log(activeTab);
   return (
     <div className="contact_container">
       {/* Carousel space */}
@@ -52,41 +63,60 @@ function Contact() {
       </div>
       {/* user form space */}
       <div className="formSpace">
-        {/* <form action="" className="form">
-          <input type="text" className="input" placeholder="FISRT NAME" />
-          <input type="text" className="input" placeholder="LAST NAME" />
-          <input type="text" className="input" placeholder="COMPANY NAME" />
-          <input type="text" className="input" placeholder="PHONE NUMBER" />
-          <input type="email" className="input" placeholder="EMAIL" />
-          <input
-            type="text"
-            className="input"
-            placeholder="YOUR BUDGET RANGE"
-          />
-        </form> */}
+        {activeTab === 0 && (
+          <form action="" className="form">
+            <input type="text" className="input" placeholder="FISRT NAME" />
+            <input type="text" className="input" placeholder="LAST NAME" />
+            <input type="text" className="input" placeholder="COMPANY NAME" />
+            <input type="text" className="input" placeholder="PHONE NUMBER" />
+            <input type="email" className="input" placeholder="EMAIL" />
+            <input
+              type="text"
+              className="input"
+              placeholder="YOUR BUDGET RANGE"
+            />
+          </form>
+        )}
 
         {/* services */}
-        <div className="contactService">
-          <h2>what do you need help with?</h2>
-          <div className="chooseService">
-            {services.map((service, indx) => {
-              return (
-                <div className="serviceContainer" key={indx}>
-                  <img src={service.icon} alt={service.serviceName} />
-                  <p>{service.serviceName}</p>
-                </div>
-              );
-            })}
+        {activeTab === 1 && (
+          <div className="contactService">
+            <h2>what do you need help with?</h2>
+            <div className="chooseService">
+              {services.map((service, indx) => {
+                return (
+                  <div className="serviceContainer" key={indx}>
+                    <img src={service.icon} alt={service.serviceName} />
+                    <p>{service.serviceName}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <textarea placeholder="Any More Description?"></textarea>
           </div>
-          <textarea placeholder="Any More Description?"></textarea>
-        </div>
+        )}
+        {activeTab === 2 && (
+          <div className="projectDetails">
+            <h2>Lets talk about your needs</h2>
+            <div className="tellMore textSpace">
+              <label htmlFor="">Please tell us more about your project</label>
+              <textarea name="needs"></textarea>
+            </div>
+            <div className="deadLine textSpace">
+              <label htmlFor="">
+                When would you like your project to start?
+              </label>
+              <textarea name="deadline" className="deadLineText"></textarea>
+            </div>
+          </div>
+        )}
       </div>
       <div className="positionBtn">
         <div className="btns">
-          <button className="prevBtn">
+          <button className="prevBtn" onClick={handlePreviousCarousel}>
             <img src={prevBtn} alt="previous icon" />
           </button>
-          <button className="nextBtn">
+          <button className="nextBtn" onClick={handleNextCarousel}>
             NEXT
             <img src={nextBtn} alt="Next button icon" />
           </button>
