4950048fc48d789b1216c7fcc15acf2c9c8fe418
Reuben <treuben90@yahoo.com>
2024-05-07 02:10:50 +0300

    sttyled the contact responsiveness

--- src/Components/Testimonials&Contacts/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials&Contacts/Testimonials/Testimonials.jsx
@@ -1,7 +1,21 @@
 import "./Testimonials.css";
 
 function Testimonials() {
-  return <div className="testimonials__container"></div>;
+  return (
+    <div className="testimonials__container">
+      <p>Testimonials</p>
+      <h2>What are people Saying?</h2>
+      <p className="message">
+        Lorem ipsum dolor sit amet consectetur. Tristique massa cursus ac nisi
+        sed natoque est. Nibh arcu vitae id hac nisl. Arcu sagittis senectus dui
+        volutpat. Gravida nunc orci mi etiam. Netus vehicula vitae id gravida.
+        Eget pellentesque augue eu gravida sed. Odio venenatis varius fusce arcu
+        turpis. Cursus faucibus elit turpis etiam leo duis pretium. Tempor urna
+        tortor sem rutrum at malesuada lacus massa habitant.
+      </p>
+      <p className="testAuthor">Irene Glover</p>
+    </div>
+  );
 }
 
 export default Testimonials;
