4abaf933e706d253f4ccec5ccc4d7f2948f3f322
Reuben <treuben90@yahoo.com>
2024-05-21 18:00:38 +0300

    created the service page half way

--- src/Pages/ServicePage/Service.jsx
+++ src/Pages/ServicePage/Service.jsx
@@ -1,5 +1,8 @@
 import NavBar from "../../Components/NavBar/NavBar";
 import agencyImage from "../../assets/Images/serveimage.png";
+import createIcon from "../../assets/Images/createPNG.png";
+import marketing from "../../assets/Images/marketingPng.png";
+import maintenance from "../../assets/Images/maintenancePng.png";
 import "./Service.css";
 
 function Service() {
@@ -18,6 +21,30 @@ function Service() {
           <div className="service_image_and_icons">
             <div className="image_container">
               <img src={agencyImage} alt="Web development agency Image" />
+            </div>
+
+            <div className="icons__space">
+              <div className="create agencyIcons">
+                <img src={createIcon} alt="creating your online presence" />
+                <h4>Create your prescence</h4>
+                <p>
+                  We excel at converting your ideas into a successful web
+                  design, logos and brand.
+                </p>
+              </div>
+              <div className="marketing agencyIcons">
+                <img src={marketing} alt="Marketing and SEO strategy" />
+                <h4>Marketing & SEO Strategy</h4>
+                <p>
+                  We dont just create we also help your business reach more
+                  clients
+                </p>
+              </div>
+              <div className="maintenace agencyIcons">
+                <img src={maintenance} alt="site maintenance and updates" />
+                <h4>Maintenance & Updates</h4>
+                <p>We assist in maintaining and keep your sites up to date.</p>
+              </div>
             </div>
           </div>
         </div>
