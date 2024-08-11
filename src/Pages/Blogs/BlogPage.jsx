016e64bf27f0f9b67c616a020b5b7821274e47b4
Reuben <treuben90@yahoo.com>
2024-08-12 00:39:06 +0300

    integrated conteful CMS

--- src/Pages/Blogs/BlogPage.jsx
+++ src/Pages/Blogs/BlogPage.jsx
@@ -1,14 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 import NavShowContext from "../../context/NavShowContext";
 import BurgerMenu from "../../Components/OpenBurgerMenu/BurgerMenu";
 import Blog from "../../Components/Blog/Blog";
 import Testimony from "../../Components/Testimonials&Contacts/Testimony/Testimony";
 import Footer from "../../Components/Footer/Footer";
+import { createClient } from "contentful";
 import "./BlogPage.css";
 
 function BlogPage() {
   const { hideNav, showNav } = useContext(NavShowContext);
+  const [blogPage, SetBlogPage] = useState([]);
+
+  const client = createClient({
+    space: import.meta.env.VITE_CONTENTFUL_SPACEID_KEY,
+    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESSTOKEN_KEY,
+  });
+
+  useEffect(() => {
+    const getBlogPage = async () => {
+      try {
+        await client.getEntries().then((entries) => console.log(entries));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getBlogPage();
+  }, []);
 
   return (
     <div className="blogPage_container">
